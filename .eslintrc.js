76116b7940366d96fc78ee475514726a8485c963
LeiLei Ros <leilei.ros+dev@gmail.com>
2020-05-07 19:48:59 -0400

    Cleaned up per linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,6 @@ module.exports = {
     'react',
   ],
   rules: {
-    'react/jsx-filename-extension': [1, { 'extensions': ['.js'] }],
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
   },
 };
