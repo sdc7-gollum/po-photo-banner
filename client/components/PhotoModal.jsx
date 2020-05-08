c9d2f35fedf7ccd2809696e4cab3ae50abe85eef
LeiLei Ros <leilei.ros+dev@gmail.com>
2020-05-08 19:58:53 -0400

    Navigate photos list using arrows

--- client/components/PhotoModal.jsx
+++ client/components/PhotoModal.jsx
@@ -4,19 +4,43 @@ import PropTypes from 'prop-types';
 class PhotoModal extends React.Component {
   constructor(props) {
     super(props);
+    const { selectedPhoto } = this.props;
 
     this.state = {
-      // selectedPhoto: this.props.selectedPhoto,
+      selectedPhoto,
     };
   }
 
+  selectPhoto(photoIndex) {
+    const { photos } = this.props;
+    let index = photoIndex;
+
+    if (photoIndex < 0) {
+      index = photos.length - 1;
+    }
+    if (photoIndex === photos.length) {
+      index = 0;
+    }
+
+    this.setState({
+      selectedPhoto: index,
+    });
+  }
+
   render() {
-    const { loadModal, photos, selectedPhoto } = this.props;
+    const { loadModal, photos } = this.props;
+    const { selectedPhoto } = this.state;
+
     return (
       <div className="photo-modal">
-        <div className="photo-modal-left" />
+        <div className="photo-modal-left">
+          <div className="left-arrow" onClick={() => this.selectPhoto(selectedPhoto - 1)} role="button" onKeyPress={this.handleKeyPress} tabIndex="0" label="previous" />
+        </div>
         <div className="photo-modal-main">
           <img src={photos[selectedPhoto].url} alt="" />
+        </div>
+        <div className="photo-modal-right">
+          <div className="right-arrow" onClick={() => this.selectPhoto(selectedPhoto + 1)} role="button" onKeyPress={this.handleKeyPress} tabIndex="0" label="next" />
         </div>
         <div className="photo-modal-right">
           <button className="btn photo-modal-close" type="button" onClick={() => { loadModal(selectedPhoto); }}>&times;</button>
