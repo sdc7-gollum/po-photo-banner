5a19f8a9b6e6701435e7a6ae1ce21c3f2be79dc0
LeiLei Ros <leilei.ros+dev@gmail.com>
2020-05-14 02:22:37 -0400

    Added prod configs

---
+++ webpack.prod.js
@@ -0,0 +1,32 @@
+
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './client/index.jsx',
+  output: {
+    filename: 'photoBannerBundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
